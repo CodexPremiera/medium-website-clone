a500c39345efffa1f4fdbe8a9b6c3a14a268dfc7
Comandao <comandaoashleyken@gmail.com>
2024-02-17 18:55:37 +0800

    first commit, set-up project

---
+++ src/firebase/firebase.js
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getStorage } from "firebase/storage";
+import { getFirestore } from "firebase/firestore";
+
+
+// TODO: Add SDKs for Firebase products that you want to use
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCFrYM79qJAZy0jBl53WgwGCtptUYLQzgs",
+  authDomain: "medium-website-clone-ebe54.firebaseapp.com",
+  projectId: "medium-website-clone-ebe54",
+  storageBucket: "medium-website-clone-ebe54.appspot.com",
+  messagingSenderId: "929192289464",
+  appId: "1:929192289464:web:e741b5e3c687727c7a404d",
+  measurementId: "G-T690TS6G9N"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth();
+export const provider = new GoogleAuthProvider();
+export const storage = getStorage();
+export const db = getFirestore(app);
