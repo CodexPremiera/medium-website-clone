53a2aedb3303554a6529e6b40c0b332f489008b3
Comandao <comandaoashleyken@gmail.com>
2024-02-18 01:31:39 +0800

    update demo header bar

--- src/components/demo/DemoHeader.jsx
+++ src/components/demo/DemoHeader.jsx
@@ -17,28 +17,36 @@ const DemoHeader = () => {
     window.addEventListener("scroll", scrollTripwire);
   }, []);
 
+  const style = {
+    header_bar: `border-b border-black sticky top-0 z-50 ${isActive ? "bg-white" : "bg-banner"} transition-all duration-500`,
+    container: `size h-[70px] flex items-center justify-between`,
+    logo: `h-[2.5rem]`,
+    nav_menu: `flex items-center gap-5`,
+    nav_list: `hidden text-sm sm:flex items-center gap-5`,
+    btn_sign_in: `hidden text-sm sm:flex items-center gap-5`,
+    btn_get_started: `text-white rounded-full px-3 py-2 text-sm font-medium ${isActive ? "bg-green-700" : "bg-black"}`
+  }
+
   return (
-    <header className={`border-b border-black sticky top-0 z-50 ${isActive ? "bg-white" : "bg-banner"} transition-all duration-500`}>
-      <div className={`size h-[70px] flex items-center justify-between`}>
+    <header className={style.header_bar}>
+      <div className={style.container}>
+
         <Link to={"/"}>
           <img
-            className={`h-[2.5rem]`}
-            src={`${Logo}`}
-            alt={"logo"}
+            className={style.logo} aria-label={`Logo`} title={`Logo`} src={`${Logo}`} alt={"logo"}
           />
         </Link>
 
-        <nav className={`flex items-center gap-5`}>
-          <div className={`hidden text-sm sm:flex items-center gap-5`}>
+        <nav className={style.nav_menu}>
+          <div className={style.nav_list}>
             {nav.map((link, index) => (
-              <Link key={index} to={link.path}>{link.title}</Link>
+              <Link key={index} to={link.path} aria-label={link.title}>{link.title}</Link>
             ))}
           </div>
-          <button className={`hidden text-sm sm:flex items-center gap-5`}>Sign In</button>
-          <button className={`text-white rounded-full px-3 py-2 text-sm font-medium ${isActive ? "bg-green-700" : "bg-black"}`}>
-              Get Started
-          </button>
+          <button className={style.btn_sign_in} aria-label={`Sign In`}>Sign In</button>
+          <button className={style.btn_get_started} aria-label={`Get Started`}>Get Started</button>
         </nav>
+
       </div>
     </header>
   );
